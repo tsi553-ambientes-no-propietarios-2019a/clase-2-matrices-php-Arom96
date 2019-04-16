8cf488dbab561d2b804672da6214e79b741d8e77
Aaron <aaron.cruz@epn.edu.ec>
2019-04-15 23:27:39 -0500

    No imprime el nombre de las columnas

---
+++ matriz.php
@@ -0,0 +1,36 @@
+<?php
+     $arr=array("Nombre"=>array("Nombre"=>"Carlos",
+                                "Apellido"=>"Granda",
+                                "Edad"=>32),
+             "Apellido"=>array("Nombre"=>"Juan",
+                               "Apellido"=>"Martinez",
+                               "Edad"=>31),
+             "Edad"=>array("Nombre"=>"María",
+                           "Apellido"=>"Jimenez",
+                           "Edad"=>22)
+  );
+
+?>
+
+
+<body>
+
+  <table width=”100%” border=”1″>
+    <?php
+          foreach($arr as $key => $value){         
+?>
+  <tr>
+      <?php foreach($value as $key=>$value){
+?>
+  <td>
+    <?php echo $value;?>
+</td>
+  <?php
+}
+?>
+  </tr>
+    <?php
+}
+?>
+  </table>
+</body>
